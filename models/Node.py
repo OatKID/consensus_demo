adc962df48cfbd605b8e123a93586ccafac80927
OatKID <pasawang.sivakorn@gmail.com>
2024-10-30 16:54:49 +0900

    Update model and create test-folder

--- models/Node.py
+++ models/Node.py
@@ -5,6 +5,7 @@ class Node:
         self.idUser = idUser
         self.timestamp = timestamp
         self.blockchain = Blockchain()
+        self.is_leader = False
     
     def add_block(self, transaction:str, timestamp:str) -> Block:
         new_block = self.blockchain.add_block(transaction, timestamp)
@@ -18,3 +19,9 @@ class Node:
 
     def receive_decide(self, messages:str):
         pass
+
+    def setLeader(self):
+        self.is_leader = True
+    
+    def unsetLeader(self):
+        self.is_leader = False
