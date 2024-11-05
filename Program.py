ada44c30699126eec0f16a48bc46b87a8097aded
OatKID <pasawang.sivakorn@gmail.com>
2024-11-05 15:21:01 +0900

    Make method to broadcast other peer

--- Program.py
+++ Program.py
@@ -1,6 +1,6 @@
 from typing import Union
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from pydantic import BaseModel
 import requests
 
@@ -11,5 +11,31 @@ class RequestTransaction(BaseModel):
 app = FastAPI()
 
 @app.post("/requestTransaction")
-def request_transaction(request:RequestTransaction):
-    return request
+def request_transaction(request:RequestTransaction, r:Request):
+    ports = [8001, 8002, 8003, 8004]
+    current_port = r.url.port
+    package = {
+        "idUser" : request.idUser,
+        "transaction": request.transaction
+    }
+    print(f"Port {current_port} receives a request from the client.")
+    for i in range(len(ports)):
+        if current_port != ports[i]:
+            try:
+                respone = requests.post(f"http://localhost:{ports[i]}/ans", json=package)
+                print(f"Port {ports[i]} is OK.")
+            except:
+                print(f"Port No.{ports[i]} is not active.")
+
+    print("Complete")
+
+    return {
+        "idUser" : request.idUser,
+        "transaction": request.transaction,
+        "verify": "OK"
+    }
+
+@app.post("/ans")
+def ans(request:RequestTransaction):
+    print(request)
+    return "OK"
--- README.md
+++ README.md
@@ -1,5 +1,20 @@
+# Consensus Algorithm For Education
+
+
+## Prepare package before execute
+1. Install [Python](https://www.python.org/) version 3.13
+2. Create virtual environment and install package as follow:
+```command line
+python -m venv .venv
+
+# window
+.\.venv\Scripts\activate.bat
+
+python install -r requirement.txt
+```
+
 ## How to run server
 ```command line
-uvicorn Program:app --host 0.0.0.0n --port 8000
+uvicorn Program:app --host 0.0.0.0 --port 8000
 ```
 > You can change Ip address of host and the number of port
