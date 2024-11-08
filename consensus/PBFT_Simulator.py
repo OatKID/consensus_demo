459590594a87d65320cb27a8036e3b7a4b6ea645
OatKID <pasawang.sivakorn@gmail.com>
2024-11-08 16:10:50 +0900

    Add faulty node

--- consensus/PBFT_Simulator.py
+++ consensus/PBFT_Simulator.py
@@ -10,10 +10,10 @@ from models.Node import Node
 import random
 from datetime import datetime
 class PBFT_Simulator:
-    def __init__(self, num_node:int) -> None:
+    def __init__(self, num_node:int, num_faulty:int=0) -> None:
+        self.num_faulty = num_faulty
         self.nodes = self.generate_nodes(num_node)
-        # Let choose the leader node to receive a request by using index
-        self.primary_node_index = random.randint(0, len(self.nodes)-1)
+        
     
     def generate_nodes(self, num_node:int):
         new_nodes = []
@@ -22,12 +22,25 @@ class PBFT_Simulator:
                 "node" : Node(i, str(datetime.now())),
                 "send_messages_log": [],
                 "receive_messages_log": [],
+                "faulty": False
             }
 
             new_nodes.append(new_node)
+
+        if self.num_faulty == 0:
+            return new_nodes
+    
+        faulty_nodes = random.sample(new_nodes, k=self.num_faulty)
+        for f_node in faulty_nodes:
+            f_node["faulty"] = True
         return new_nodes
 
     def receive_request(self, message):
+        # Let choose the leader node to receive a request by using index
+        self.primary_node_index = random.randint(0, len(self.nodes)-1)
+        while self.nodes[self.primary_node_index]["faulty"]:
+            self.primary_node_index = random.randint(0, len(self.nodes)-1)
+        
         recieve_message = (message, "request", -1) # -1 define to be the client which send a request
         self.nodes[self.primary_node_index]["receive_messages_log"].append(recieve_message)
     
@@ -48,7 +61,7 @@ class PBFT_Simulator:
         prepare_message = (message, "prepare", current_node["node"].idUser)
         current_node["send_messages_log"].append(prepare_message)
         for i in range(len(self.nodes)):
-            if self.nodes[i] != current_node:
+            if (self.nodes[i] != current_node):
                 self.receive_prepare(prepare_message, i)
     
     def receive_prepare(self, message, index):
@@ -63,16 +76,20 @@ class PBFT_Simulator:
     def send_request(self, new_transaction:str):
 
         # Start to recieve a request from the client
+        print("Request Phase")
         self.receive_request(new_transaction)
         self.get_nodes()
 
-        # The leader node broadcasts to the other nodes (Pre-Prepare phase)
+        # The leader node broadcasts to the other nodes (Pre-Prepare Phase)
+        print("Pre-Prepare Phase")
         self.broadcast_pre_prepare()
         self.get_nodes()
 
+        # Other nodes which exclude the leader node will broadcast other nodes (Prepare Phase)
+        print("Prepare Phase")
         for node in self.nodes:
-            if node != self.nodes[self.primary_node_index]:
+            if node != self.nodes[self.primary_node_index] and (node["faulty"] == False):
                 self.broadcast_prepare(node)
-        
         self.get_nodes()
 
+        # After each node have received prepare messages already, it will broadcast commit messages to make new block (Assume that the message is true)
--- simulator.py
+++ simulator.py
@@ -1,5 +1,8 @@
 from consensus.PBFT_Simulator import PBFT_Simulator
 
-consensus = PBFT_Simulator(10)
+consensus = PBFT_Simulator(10, 2)
+
+# for node in consensus.nodes:
+#     print(node)
 
 consensus.send_request("Hello world")
