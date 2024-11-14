cb2c1d434540b136cf47dd33c9d83da600944090
OatKID <pasawang.sivakorn@gmail.com>
2024-11-14 11:56:35 +0900

    Edit script to execute easily

--- simulator.bat
+++ simulator.bat
@@ -1,3 +1,16 @@
 @echo off
 
-python simulator.py > output.txt
+echo PBFT
+python simulator.py pbft 10 2 > pbft_result.txt
+pause
+
+echo QPBFT
+python simulator.py qpbft 6 4 2 > qpbft_result.txt
+type -Tail 1 qpbft_result.txt
+pause
+
+echo Proposed
+python simulator.py proposed 6 4 5 2 > proposed_result.txt
+pause
+
+echo Finish
